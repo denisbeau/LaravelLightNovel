83f7161e70d3151993c6640470c060d519ffaea4
Raffi Boyajian <2279458@cmontmorency.qc.ca>
2025-10-31 00:02:05 -0400

    Ajout de vérification mailtrap.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\View\View;
+use Illuminate\Routing\Redirector;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use App\Http\Controllers\LightNovelController;
 use App\Http\Controllers\CommentaireController;
 // use App\Http\Controllers\ArticleController; // <-- commenter si ArticleController n'existe pas
@@ -20,5 +26,20 @@ Route::resource('commentaires', CommentaireController::class);
 Route::get('/light_novels/autocomplete', [App\Http\Controllers\LightNovelController::class, 'autocomplete'])->name('light_novels.autocomplete');
 
 Auth::routes();
+Auth::routes(['verify' => true]);
+
+Route::get('/email/verify', function (): Factory|View {
+    return view('auth.verify');
+})->middleware(['auth'])->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request): Redirector|RedirectResponse {
+    $request->fulfill();
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request): RedirectResponse {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
